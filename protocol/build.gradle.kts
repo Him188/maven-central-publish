f941479445f07b1e2914fa45ad808ba51668b905
Him188 <Him188@mamoe.net>
2021-11-29 03:28:50 +0000

    Use default Kotlin compiler settings for protocol module

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
-
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
@@ -14,15 +12,6 @@ kotlin {
             dependencies {
                 api("org.jetbrains.kotlinx:kotlinx-serialization-core:${extra.get("serialization")}")
                 api("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:${extra.get("serialization")}")
-            }
-        }
-    }
-
-    targets.filter { it.platformType != KotlinPlatformType.native }.forEach { target ->
-        target.compilations.all {
-            kotlinOptions {
-                apiVersion = "1.3"
-                languageVersion = "1.4"
             }
         }
     }
