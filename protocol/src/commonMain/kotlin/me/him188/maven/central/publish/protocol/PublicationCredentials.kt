629b2d1177b08f353d4ded1d3179a24fb07733a9
eritque0arcus <eritque-arcus@eritque-arcus.tech>
2023-09-04 12:19:09 -0400

    fix: downgrade serialization version for MPP

--- protocol/src/commonMain/kotlin/me/him188/maven/central/publish/protocol/PublicationCredentials.kt
+++ protocol/src/commonMain/kotlin/me/him188/maven/central/publish/protocol/PublicationCredentials.kt
@@ -1,11 +1,12 @@
 package me.him188.maven.central.publish.protocol
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.protobuf.ProtoNumber
 
 @Suppress("EXPERIMENTAL_API_USAGE")
 @Serializable
-data class PublicationCredentials(
+data class PublicationCredentials @OptIn(ExperimentalSerializationApi::class) constructor(
     @ProtoNumber(1) val gpgPublicKey: String,
     @ProtoNumber(2) val gpgPrivateKey: String,
     @ProtoNumber(3) val sonatypeUsername: String,
